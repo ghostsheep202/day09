47641d8743ac84b1d52beb2785c619d4aa56aba9
z1246440544 <z1246440544@63.com>
2020-06-27 20:13:32 +0800

    111

---
+++ scripts/test_step.py
@@ -0,0 +1,24 @@
+import allure
+
+class TestSetup:
+    @allure.step("我是测试步骤名称")
+    def test_001(self):
+        """添加测试步骤"""
+        #添加测试步骤描述,生成一个附件
+        allure.attach("我是步骤描述得内容","附件名字")
+
+    @allure.severity(allure.severity_level.CRITICAL)
+    def test_002(self):
+        assert False
+
+    @allure.severity(allure.severity_level.NORMAL)
+    def test_003(self):
+        assert True
+
+    @allure.severity(allure.severity_level.MINOR)
+    def test_004(self):
+        assert True
+
+    @allure.severity(allure.severity_level.TRIVIAL)
+    def test_005(self):
+        assert True
